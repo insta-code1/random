98b917832b19b323c52ea3aa079f2008fbf1a0ee
Eoin <eoinkane1@gmail.com>
2016-04-22 15:41:52 +0100

    urls

--- we_are_social/urls.py
+++ we_are_social/urls.py
@@ -23,6 +23,7 @@ from magazines import views as magazine_views
 from accounts.views import register, profile, login, logout, cancel_subscription, subscriptions_webhook
 from threads import views as forum_views
 from polls import api_views
+from django.contrib.staticfiles import views as static_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -61,4 +62,7 @@ urlpatterns = [
 
     # Rest API
     url(r'^threads/polls/$', api_views.PollViewSet.as_view()),
+
+    #static urls
+    url(r'^static/(?P<path>.*)$', static_views.serve),
 ]
